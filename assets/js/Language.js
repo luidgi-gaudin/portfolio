34d138b4912fa8dd17644499f42a115bf0ce84f4
luidgi <146814537+luidgi-gaudin@users.noreply.github.com>
2024-02-01 23:37:55 +0100

    lang

---
+++ assets/js/Language.js
@@ -0,0 +1,9 @@
+// Get the select element
+const selectLanguage = document.querySelector(".select-language");
+
+// Add event listener to detect changes in the select element
+selectLanguage.addEventListener("change", function () {
+  // Check if the selected value is "english"
+  if (this.value === "en") {
+  }
+});
